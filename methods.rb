f0386248fb290eab1c2b41af70127ce67072b6f9
August <auggierutter@gmail.com>
2021-09-02 12:20:56 -0400

    commit tree 14c38fe898c065f2a6615379446df6111a64fbec

--- methods.rb
+++ methods.rb
@@ -1,1 +1,26 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet(name="programmer")
+    puts "Hello, #{name}!"
+end 
+greet "Naureen"
+
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+end 
+greet_with_default "Naureen"
+
+def add(num1,num2)
+    return num1 + num2
+end
+add(2,5)
+
+def halve(num)
+return nil if num = String
+else 
+    if num = Integer 
+        return num/2
+end
